2d2a176b4eb4701ecae09b746288bb84c8e6203d
Puranjay Yadav <puranjayrocks4@gmail.com>
2020-08-05 15:14:24 +0530

    Finished Error Handling

--- Controller/errorController.js
+++ Controller/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('./../appError');
+
+const handleCastErrorDB = err =>{
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message,400);
+}
 const sendErrorDev = (err,res)=>{
 
     res.status(err.statusCode).json({
@@ -8,10 +14,21 @@ const sendErrorDev = (err,res)=>{
     });
 }
 const sendErrorProd = (err,res)=>{
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    }else{
+
+        // 1)Log Error
+        console.log('Error' , err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        })
+    }
+   
 }
 
 
@@ -23,6 +40,8 @@ module.exports = (err,req,res,next) =>{
     if(process.env.NODE_ENV === 'development'){
       sendErrorDev(err,res);
     }else if(process.env.NODE_ENV === 'production'){
-     sendErrorProd(err,res);
+        let error = {...err};
+        if(error.name === 'CastError') error = handleCastErrorDB(error);
+     sendErrorProd(error,res);
      }
 };
